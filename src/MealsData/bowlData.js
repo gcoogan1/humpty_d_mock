ee3c63f82efc1f3d0860aa76ece3cd24e814b1a4
Genevieve Coogan <gcoogan1@gmail.com>
2020-12-20 17:59:30 -0500

    updated photos

--- src/MealsData/bowlData.js
+++ src/MealsData/bowlData.js
@@ -1,17 +1,22 @@
 
 import Star from '../assets/star.png';
-import Noodles from '../assets/food/noodles-placeholder.png';
+//import Noodles from '../assets/food/noodles-placeholder.png';
+import BuffaloChicken from "../assets/food/recents/noodles/buffalo-chicken-noodles.png";
+import Cheesesteak from "../assets/food/recents/noodles/cheesesteak-noodles.png";
+import ChickenPotPie from "../assets/food/recents/noodles/chicken-pot-pie-noodles.png";
+import HoneyChipotle from "../assets/food/recents/noodles/honey-chipotle-chicken-noodles.png";
+import ItalianRoast from "../assets/food/recents/noodles/italian-roast-pork-noodles.png";
 
 
 
 
 const bowls = [
-    { "id": 1, "meal":"Buffalo Chicken", "image": Noodles, "vegan" : "", "star": "", "info": "Crispy chicken bites and Humpty's Mild Wing sauce. garnished with blue cheese crumbles and a drizzle of ranch."},
-    { "id": 2, "meal":"Chicken Pot Pie", "image": Noodles, "vegan" : "", "star": "","info": "Chicken breast, mixed veggies, homemade gravy. topped with pie crust crumbles and shredded carrots."},
-    { "id": 3, "meal":"Cheesesteak", "image": Noodles, "vegan" : "", "star": "","info": "Chip steak, peppers, onions, cheese sauce. garnished with scallions."},
-    { "id": 4, "meal":"Italian Roast Pork", "image": Noodles, "vegan" : "", "star": "","info": "Pork loin, broccoli rabe, sharp provolone with our own housemade herb and garlic sauce."},
-    { "id": 5, "meal":"Honey Chipotle Chicken", "image": Noodles, "vegan" : "", "star": "", "info": "Sauteed onions and crispy chicken tossed in Humpty's Honey BBQ sauce with a ranch drizzle and scallion garnish."},
-    { "id": 6, "meal":"Honey Sriracha Chicken", "image": Noodles, "vegan" : "", "star": Star ,"info": "Honey Lime Sriracha Sauce, Red Cabbage, Brussels Sprouts, Carrots, and cripy chicken nuggets garnished w/ sesame seeds.."},
+    { "id": 1, "meal":"Buffalo Chicken", "image": BuffaloChicken, "vegan" : "", "star": "", "info": "Crispy chicken bites and Humpty's Mild Wing sauce. garnished with blue cheese crumbles and a drizzle of ranch."},
+    { "id": 2, "meal":"Chicken Pot Pie", "image": ChickenPotPie, "vegan" : "", "star": "","info": "Chicken breast, mixed veggies, homemade gravy. topped with pie crust crumbles and shredded carrots."},
+    { "id": 3, "meal":"Cheesesteak", "image": Cheesesteak, "vegan" : "", "star": "","info": "Chip steak, peppers, onions, cheese sauce. garnished with scallions."},
+    { "id": 4, "meal":"Italian Roast Pork", "image": ItalianRoast, "vegan" : "", "star": "","info": "Pork loin, broccoli rabe, sharp provolone with our own housemade herb and garlic sauce."},
+    { "id": 5, "meal":"Honey Chipotle Chicken", "image": HoneyChipotle, "vegan" : "", "star": "", "info": "Sauteed onions and crispy chicken tossed in Humpty's Honey BBQ sauce with a ranch drizzle and scallion garnish."},
+   
     
 
 ];
--- src/MealsData/dumplingData.js
+++ src/MealsData/dumplingData.js
@@ -1,6 +1,17 @@
 import Star from '../assets/star.png';
 import Vegan from '../assets/Vegan.svg';
-import Dumplings from '../assets/food/dumplings-placeholder.png'
+//import Dumplings from '../assets/food/dumplings-placeholder.png';
+//Dumplings
+import AsianPork from '../assets/food/recents/dumplings/asian-pork-dumpling.png';
+import BroccoliChedder from '../assets/food/recents/dumplings/broccoli-cheddar-dumpling.png';
+import BuffaloChicken from '../assets/food/recents/dumplings/buffalo-chicken-dumpling.png';
+import CheeseSteak from '../assets/food/recents/dumplings/cheesesteak-dumpling.png';
+import HoneyChipotle from '../assets/food/recents/dumplings/honey-chipotle-chicken-dumpling.png';
+import ItalianRoast from '../assets/food/recents/dumplings/italian-roast-dumpling.png';
+import MedCauliflower from '../assets/food/recents/dumplings/mediterranean-cauliflower-dumpling.png';
+import MixedBerry from '../assets/food/recents/dumplings/mixed-berry-cheesecake-dumpling.png';
+import SpinachMozz from '../assets/food/recents/dumplings/spinach-mozzarella-ricotta-dumpling.png';
+
 
 
 //TODO:
@@ -8,14 +19,15 @@ import Dumplings from '../assets/food/dumplings-placeholder.png'
 
 
 const dumplings = [
-    { "id": 1, "meal":"Asian Pork", "image": Dumplings, "vegan" : "", "star": "", "info": "If you want traditional, this your stop..Pork shoulder, ginger, soy, scallions."},
-    { "id": 2, "meal":"Broccoli & Cheddar", "image": Dumplings, "vegan" : Vegan, "star": "","info": "Only two ingredients in this classic, if you enjoy this casserole dish you'll love the dumplings."},
-    { "id": 3, "meal":"Buffalo Chicken", "image": Dumplings, "vegan" : "", "star": "","info": "Like a boneless wing. Only better! mild heat."},
-    { "id": 4, "meal":"Honey Chipotle Chicken", "image": Dumplings, "vegan" : "", "star": "","info": "Chicken breast, smoked gouda, red onions, scallions, honey chipotle bbq sauce. Think sweet and smokey."},
-    { "id": 5, "meal":"Cheesteak", "image": Dumplings, "vegan" : "", "star": "", "info": "Chip steak, american cheese."},
-    { "id": 6, "meal":"Vegan Burrito", "image": Dumplings, "vegan" : Vegan, "star": Star ,"info": "Rice, beans, corn, red bell pepper, cilantro & lime."},
-    { "id": 7, "meal":"Spinach Mozzarella & Ricotta", "image": Dumplings, "vegan" : Vegan, "star": Star, "info": "Vegetarian."},
-    { "id": 8, "meal":"Apple Pie", "image": Dumplings, "vegan" : "", "star": Star, "info": "Good old fashioned apple pie filling, exterior coated in cinnamon sugar."},
+    { "id": 1, "meal":"Asian Pork", "image": AsianPork, "vegan" : "", "star": "", "info": "If you want traditional, this your stop..Pork shoulder, ginger, soy, scallions."},
+    { "id": 2, "meal":"Broccoli & Cheddar", "image": BroccoliChedder, "vegan" : Vegan, "star": "","info": "Only two ingredients in this classic, if you enjoy this casserole dish you'll love the dumplings."},
+    { "id": 3, "meal":"Buffalo Chicken", "image": BuffaloChicken, "vegan" : "", "star": "","info": "Like a boneless wing. Only better! mild heat."},
+    { "id": 4, "meal":"Honey Chipotle Chicken", "image": HoneyChipotle, "vegan" : "", "star": "","info": "Chicken breast, smoked gouda, red onions, scallions, honey chipotle bbq sauce. Think sweet and smokey."},
+    { "id": 5, "meal":"Cheesesteak", "image": CheeseSteak, "vegan" : "", "star": "", "info": "Chip steak, american cheese."},
+    { "id": 6, "meal":"Mixed Berry Cheesecake", "image": MixedBerry, "vegan" : Vegan, "star": Star ,"info": "Raspberry, blueberry, blackberry in a cheesecake filling."},
+    { "id": 7, "meal":"Spinach Mozzarella & Ricotta", "image": SpinachMozz, "vegan" : Vegan, "star": Star, "info": "Spinach Mozzarella & Ricotta."},
+    { "id": 8, "meal":"Mediterranean Roasted Cauliflower", "image": MedCauliflower, "vegan" : Vegan, "star": Star, "info": "Roasted cauliflower & carrots, rose harissa, pomegranate, and cilantro."},
+    { "id": 9, "meal":"Italian Roast Pork", "image": ItalianRoast, "vegan" : "", "star": Star, "info": "Pork loin, broccoli rabe, herb & garlic sauce w/ imported aged provolone."},
 
 ];
 
